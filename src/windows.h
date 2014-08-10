daa1ccbfbc9f2e252b4e2b4f7375bd50983024ed
OHASHI Hideya <ohachige@gmail.com>
2014-08-10 23:55:03 +0900

    use SSE3

--- src/windows.h
+++ src/windows.h
@@ -1,8 +1,11 @@
 #include <stdint.h>
+#include <stdlib.h>
 #include <string.h>
 #include <algorithm>
+#include <xmmintrin.h>
+#include <pmmintrin.h>
 using std::min;
 using std::max;
 typedef uint32_t DWORD;
-#define _aligned_malloc(a,b) malloc(a)
-#define _aligned_free(a) free(a)
+#define _aligned_malloc(a,b) aligned_alloc(b,a)
+#define _aligned_free(a)
